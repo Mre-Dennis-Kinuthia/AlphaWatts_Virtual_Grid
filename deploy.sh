dce3ede588aec0212b6596b9de9777e5d821bb54
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2022-12-22 12:03:10 +0000

    Initializing

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "============initilization============="
+git add .
+
+git commit -m "Initializing"
+
+git push origin master
+
+echo "============Deploye==================="
+
+clear
